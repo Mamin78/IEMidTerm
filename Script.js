2f5e4c5dcd55eb705459c17c49ab7e1eed1955ef
Mamin78 <aminsh78@gmail.com>
2021-04-15 17:13:03 +0430

    Now, we can save gender of user and return it

--- Script.js
+++ Script.js
@@ -3,6 +3,52 @@ async function handleSubmitClick(name) {
         `https://api.genderize.io/?name=${name}`,
     ).then((reponse) => reponse.json());
     console.log(result);
+
+    document.getElementsByClassName("right")[0].style.display = 'block';
+    document.getElementById("prediction-gender").innerHTML = result['gender'];
+
+    document.getElementById("prediction-prob").innerHTML = result['probability'];
+
+    document.getElementsByClassName("left")[0].style.borderRight = '1px dashed #636e72';
+    // document.getElementsByClassName("left")[0].style.marginRight = '1vw';
+    document.getElementsByClassName("left")[0].style.paddingRight = '1.25vw';
+    document.getElementById("prediction").style.display = 'block';
+
+
+    var inputGender = document.getElementsByName("gender");
+
+    if (inputGender[0].checked || inputGender[1].checked) {
+        if (inputGender[0].checked) {
+            document.getElementById("saved-answer-gender").innerHTML = 'male';
+            document.getElementById("saved-answer").style.display = 'block';
+
+            setItem(name, "male");
+        } else if(inputGender[1].checked){
+            document.getElementById("saved-answer-gender").innerHTML = 'female';
+            document.getElementById("saved-answer").style.display = 'block';
+
+            setItem(name, "female");
+        }
+    } else {
+        var savedGender = getItem(name);
+        if (savedGender) {
+            document.getElementById("saved-answer-gender").innerHTML = savedGender;
+            document.getElementById("saved-answer").style.display = 'block';
+        }
+    }
+
+}
+
+function getItem(key) {
+    return JSON.parse(localStorage.getItem(key));
+}
+
+function setItem(key, value) {
+    localStorage.setItem(key, JSON.stringify(value));
+}
+
+function removeItem(key) {
+    localStorage.removeItem(key);
 }
 
 const input = document.querySelector('#name');

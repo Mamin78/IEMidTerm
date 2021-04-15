cd1e04f11a164a93a0e07fffea34f4792f47def4
Mamin78 <aminsh78@gmail.com>
2021-04-15 19:09:37 +0430

    The code works correct

--- Script.js
+++ Script.js
@@ -1,9 +1,21 @@
-async function handleSubmitClick(name) {
-    const result = await fetch(
-        `https://api.genderize.io/?name=${name}`,
-    ).then((reponse) => reponse.json());
-    console.log(result);
+let personGender = {
+    _name: "",
+    _gender: "",
+    get name() {
+        return this._name;
+    },
+    get gender() {
+        return this.gender;
+    },
+    set name(inputName) {
+        this._name = inputName;
+    },
+    set gender(inputGender) {
+        this._gender = inputGender;
+    },
+};
 
+function setPrediction(result) {
     document.getElementsByClassName("right")[0].style.display = 'block';
     document.getElementById("prediction-gender").innerHTML = result['gender'];
 
@@ -13,30 +25,82 @@ async function handleSubmitClick(name) {
     // document.getElementsByClassName("left")[0].style.marginRight = '1vw';
     document.getElementsByClassName("left")[0].style.paddingRight = '1.25vw';
     document.getElementById("prediction").style.display = 'block';
+}
 
+function setSavedAnswer(inputGender) {
+    document.getElementById("saved-answer-gender").innerHTML = inputGender;
+    document.getElementById("saved-answer").style.display = 'block';
+}
 
-    var inputGender = document.getElementsByName("gender");
-
-    if (inputGender[0].checked || inputGender[1].checked) {
-        if (inputGender[0].checked) {
-            document.getElementById("saved-answer-gender").innerHTML = 'male';
-            document.getElementById("saved-answer").style.display = 'block';
-
-            setItem(name, "male");
-        } else if(inputGender[1].checked){
-            document.getElementById("saved-answer-gender").innerHTML = 'female';
-            document.getElementById("saved-answer").style.display = 'block';
-
-            setItem(name, "female");
-        }
+async function handleSubmitClick(name) {
+    if (name === "") {
+        window.alert("Please write your name.");
     } else {
-        var savedGender = getItem(name);
-        if (savedGender) {
-            document.getElementById("saved-answer-gender").innerHTML = savedGender;
-            document.getElementById("saved-answer").style.display = 'block';
+        const result = await fetch(
+            `https://api.genderize.io/?name=${name}`,
+        ).then((reponse) => reponse.json());
+
+        console.log(result);
+
+        if (result['gender'] === null) {
+            // if input name isn't in database we should alert!
+            window.alert("Your name isn't in our DataBase.!");
+        } else {
+            setPrediction(result);
+
+            const inputGender = document.getElementsByName("gender");
+
+            if (inputGender[0].checked || inputGender[1].checked) {
+                console.log("radio ddddddddddddddddddddddddd");
+                if (getItem(name) !== null) {
+                    removeItem(name);
+                }
+                if (inputGender[0].checked) {
+                    setSavedAnswer('male');
+
+                    personGender._name = name;
+                    personGender._gender = "male";
+
+                    setItem(name, "male");
+
+                    console.log("first if");
+                    document.getElementById("male").checked = false;
+                } else if (inputGender[1].checked) {
+                    setSavedAnswer('female');
+
+                    personGender._name = name;
+                    personGender._gender = "female";
+
+                    setItem(name, "female");
+
+                    console.log("second if");
+                    document.getElementById("female").checked = false;
+                }
+            } else {
+                const savedGender = getItem(name);
+                if (savedGender !== null) {
+                    setSavedAnswer(savedGender);
+                    personGender._name = name;
+                    personGender._gender = '';
+
+                    console.log("third if");
+                } else {
+                    document.getElementById("saved-answer").style.display = 'none';
+
+                }
+            }
         }
     }
+}
 
+function handleRemoveClick() {
+    // if (getItem(personGender._name) !== null) {
+    removeItem(personGender._name);
+    personGender._name = '';
+    personGender._gender = '';
+    document.getElementById("saved-answer-gender").innerHTML = getItem(personGender._name);
+    document.getElementById("saved-answer").style.display = 'none';
+    // }
 }
 
 function getItem(key) {
@@ -54,6 +118,13 @@ function removeItem(key) {
 const input = document.querySelector('#name');
 const submitBtn = document.querySelector('#submit_btn');
 submitBtn.addEventListener('click', (e) => {
-    e.preventDefault()
-    handleSubmitClick(input.value)
+    e.preventDefault();
+    handleSubmitClick(input.value);
+})
+
+
+const removeBtn = document.querySelector('#remove-btn');
+removeBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    handleRemoveClick();
 })
